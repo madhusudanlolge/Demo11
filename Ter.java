7dccfc1aa8f4e7358a681b64f5ecf007d8fa2b04
madhusudanlolge <101565450+madhusudanlolge@users.noreply.github.com>
2022-03-15 17:58:00 +0530

    ternary

---
+++ Ter.java
@@ -0,0 +1,14 @@
+package Operatora;
+
+public class Ter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a =10.3f;
+		float b= 10.4f;
+		float c = a>=b?a:b;
+		System.out.println(c);
+
+	}
+
+}
