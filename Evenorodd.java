e4dbb088d6b32c5c1e490e6a8f7e7fcab00c25d9
madhusudanlolge <101565450+madhusudanlolge@users.noreply.github.com>
2022-03-15 18:20:37 +0530

    even

---
+++ Evenorodd.java
@@ -0,0 +1,16 @@
+package Pack;
+
+
+public class Evenorodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		if(a%2==0)  // % operater for reminder  
+			System.out.println("Even Number");
+		else 
+			System.out.println("Odd Number");
+		
+	}
+
+}
